3fba0b6cc79ea47ae0f5ba888f15b2924e0030a6
codewithdarkwa <jnrdrkw1@gmail.com>
2023-05-15 20:55:52 +0000

    update

--- lib/views/call_screen.dart
+++ lib/views/call_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/call_model.dart';
+
 class CallScreen extends StatefulWidget {
   const CallScreen({super.key});
 
@@ -10,8 +12,46 @@ class CallScreen extends StatefulWidget {
 class _CallScreenState extends State<CallScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('CallScreen'),
-    );
+    return ListView.builder(
+        itemCount: callList.length,
+        itemBuilder: (context, index) {
+          final call = callList[index];
+          return Column(children: [
+            const Divider(thickness: 1.2, height: 5),
+            InkWell(
+              onTap: () {
+                //initiate the call
+              },
+              child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: NetworkImage(call.imgurl),
+                  ),
+                  title: Text(
+                    call.name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Container(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Row(
+                      children: [
+                        call.status,
+                        Text(
+                          call.time,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {},
+                      icon: Icon(call.type.icon,
+                          color: Theme.of(context).primaryColor))),
+            ),
+          ]);
+        });
   }
 }
--- lib/views/status_screen.dart
+++ lib/views/status_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/chat_model.dart';
+
 class StatusScreen extends StatefulWidget {
   const StatusScreen({super.key});
 
@@ -10,8 +12,77 @@ class StatusScreen extends StatefulWidget {
 class _StatusScreenState extends State<StatusScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('StatusScreen'),
+    return Container(
+      color: const Color(0xfff2f2f2),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+            color: Colors.white,
+            elevation: 0.0,
+            child: ListTile(
+              leading: Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                        "https://s3.amazonaws.com/wll-community-production/images/no-avatar.png"),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 1,
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: const BoxDecoration(
+                        color: Colors.green,
+                        shape: BoxShape.circle,
+                      ),
+                      child:
+                          const Icon(Icons.add, color: Colors.white, size: 15),
+                    ),
+                  ),
+                ],
+              ),
+              title: const Text(
+                'My Status',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: const Text('Tap to add status update'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Viewed updates',
+                style:
+                    TextStyle(color: Colors.grey, fontWeight: FontWeight.bold)),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.white,
+              child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    final chat = chatList[index];
+                    return ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(chat.avatarUrl),
+                      ),
+                      title: Text(
+                        chat.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: const Text('Today, 7:15 PM'),
+                    );
+                  },
+                  separatorBuilder: (context, _) =>
+                      const Padding(padding: EdgeInsets.only(top: 5)),
+                  itemCount: chatList.length),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
